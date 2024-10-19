2cd5d3a04e045ad6ec2e664b8b742ae54ad3d745
Nadia Albiol Moral <NALBMOR1@upv.edu.es>
2024-10-19 19:57:04 +0200

    entitie producto

--- lib/shared/data_entities/product.dart
+++ lib/shared/data_entities/product.dart
@@ -4,11 +4,15 @@ class Product
   final String nombre;
   final String foto;
   final String alergeno;
+  final double precio;
+  final String tienda;
   
   Product({
     required this.id,
     required this.nombre,
     required this.foto,
     required this.alergeno,
+    required this.precio,
+    required this.tienda,
   });
 }
