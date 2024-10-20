d10789083a699fdcf11bc2dd81db5f5faa3cb1c1
Nadia Albiol Moral <NALBMOR1@upv.edu.es>
2024-10-20 18:14:31 +0200

    cambio servicio dia finder y producto a medias

--- lib/features/dia_finder/application/dia_finder_service.dart
+++ lib/features/dia_finder/application/dia_finder_service.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../../../shared/data_entities/producto.dart';
-import 'package:html/parser.dart' as htmlParser;
 
 class DiaFinderService {
   final String marketUri = "https://www.dia.es/api/v1/search-back/search/reduced?q=%s&page=1";
@@ -25,61 +24,18 @@ class DiaFinderService {
     }
   }
 
-  // Método para analizar el HTML y extraer alérgenos en negrita
-  List<String> _extractAlergenosFromHtml(String html) {
-    final document = htmlParser.parse(html);
-    final List<String> alergenos = [];
-
-    // Busca el div que contiene los ingredientes
-    final ingredientsSection = document.querySelector("#html-container");
-    if (ingredientsSection != null) {
-      // Selecciona todos los elementos <strong> dentro de la sección de ingredientes
-      final elements = ingredientsSection.querySelectorAll("strong");
-
-      for (var element in elements) {
-        // Asegúrate de que el texto no esté vacío antes de añadirlo
-        if (element.text.isNotEmpty) {
-          alergenos.add(element.text.trim());
-        }
-      }
-    }
-
-    return alergenos;
-  }
-
-  // Función para obtener los ingredientes y alérgenos de la página del producto
-  Future<List<String>> fetchProductAlergenos(String productDetailUrl) async {
-    try {
-      final response = await http.get(Uri.parse(productDetailUrl));
-
-      if (response.statusCode == 200) {
-        return _extractAlergenosFromHtml(response.body);
-      } else {
-        print("Failed to load product details");
-        return [];
-      }
-    } catch (e) {
-      print("Error fetching product details: $e");
-      return [];
-    }
-  }
-
-  Future<List<Producto>> _parseProductList(Map<String, dynamic> json) async {
+  List<Producto> _parseProductList(Map<String, dynamic> json) {
     final List<Producto> productList = [];
     final productsJsonList = json['search_items'] as List<dynamic>;
 
+    final facets = json['facets'];
+
     for (final productJson in productsJsonList) {
-      final String productId = productJson['id'] ?? '';
-      final String productDetailUrl = "https://www.dia.es/p/$productId";
-
-      // Obtener alérgenos
-      final List<String> alergenos = await fetchProductAlergenos(productDetailUrl);
-
       final productObj = Producto.fromJson(
         productJson,
         imageHost: imageHost,
         marketName: "DIA",
-        alergenos: alergenos,
+        facets: facets,
       );
       productList.add(productObj);
     }
@@ -87,3 +43,4 @@ class DiaFinderService {
     return productList;
   }
 }
+
--- lib/shared/data_entities/producto.dart
+++ lib/shared/data_entities/producto.dart
@@ -3,7 +3,7 @@ class Producto
   final String id;
   final String nombre;
   final String foto;
-  final List<String> alergenos;
+  final List<bool> alergenos;
   final double precio;
   final String tienda;
   
@@ -16,15 +16,37 @@ class Producto
     required this.tienda,
   });
 
-  // Método para convertir un JSON a un objeto Producto, pasando el prefijo de imagen y el nombre del supermercado como parámetros
-  factory Producto.fromJson(Map<String, dynamic> json, {required String imageHost, required String marketName, List<String>? alergenos}) {
+  // Método para convertir un JSON a un objeto Producto
+  factory Producto.fromJson(Map<String, dynamic> json, {required String imageHost, required String marketName, required Map<String, dynamic> facets}) {
+    // Obtener el estado de los alérgenos
+    bool glutenFree = _getAllergenStatus(facets, 'gluten_free');
+    bool lactoseFree = _getAllergenStatus(facets, 'lactose_free');
+    bool nutsFree = _getAllergenStatus(facets, 'nuts_free');
+
     return Producto(
-      id: json['id'] ?? '',
+      id: json['object_id'] ?? '', // Utiliza el campo adecuado del JSON
       nombre: json['display_name'] ?? '',
       precio: json['prices']['price'].toDouble(),
       foto: imageHost + (json['image'] ?? ''),
       tienda: marketName,
-      alergenos: alergenos ?? [],
+      alergenos: [glutenFree, lactoseFree, nutsFree], // Asignación de alérgenos
     );
+  }
+
+  // Método para obtener el estado de un alérgeno
+  static bool _getAllergenStatus(Map<String, dynamic> facets, String allergenKey) {
+    // Busca el facet correspondiente al alérgeno
+    final allergenFacet = facets[allergenKey];
+
+    if (allergenFacet != null) {
+      // Busca el filtro habilitado
+      final filter = allergenFacet['filters']
+          .firstWhere((filter) => filter['enabled'] == true, orElse: () => null);
+
+      if (filter != null) {
+        return filter['title'] == 'SI'; // Devuelve true si el producto es libre de alérgenos
+      }
+    }
+    return false; // Retorna false si no se encuentra el estado
   }
 }
