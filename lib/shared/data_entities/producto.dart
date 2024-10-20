1c9e21095089b0177140f00dc308500aa2a68d4c
QuinoCode <joaquinangelvalienteruiz@gmail.com>
2024-10-20 18:02:10 +0200

    Added a domain model for the response of the API of Mercadona aswell as tweaked a field from Product

--- lib/shared/data_entities/producto.dart
+++ lib/shared/data_entities/producto.dart
@@ -1,9 +1,20 @@
+import 'dart:ffi';
+
 class Producto
 {
   final String id;
   final String nombre;
   final String foto;
-  final List<String> alergenos;
+  final List<bool> alergenos; 
+  /*
+  Explicación campo alérgenos
+  -----------------------------
+  Consistirá en una lista de 3 booleanos con las posiciones establecidas de la siguiente manera
+  - Posición 0: booleano para el GLUTEN
+  - Posición 1: booleano para la LACTOSA 
+  - Posición 2: booleano para los FRUTOS SECOS
+  En caso de ser 0 no contendrá este alérgeno el producto, en caso de ser uno si lo contendrá
+  */
   final double precio;
   final String tienda;
   
@@ -17,7 +28,7 @@ class Producto
   });
 
   // Método para convertir un JSON a un objeto Producto, pasando el prefijo de imagen y el nombre del supermercado como parámetros
-  factory Producto.fromJson(Map<String, dynamic> json, {required String imageHost, required String marketName, List<String>? alergenos}) {
+  factory Producto.fromJson(Map<String, dynamic> json, {required String imageHost, required String marketName, List<bool>? alergenos}) {
     return Producto(
       id: json['id'] ?? '',
       nombre: json['display_name'] ?? '',
