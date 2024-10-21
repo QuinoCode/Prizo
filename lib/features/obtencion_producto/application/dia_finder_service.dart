6435b69a823cd463b9fe1767066321554e003036
Nadia Albiol Moral <NALBMOR1@upv.edu.es>
2024-10-21 17:41:20 +0200

    arreglo servicio dia_finder y entidad producto, y creación (de forma provisional) de entidad tienda

--- lib/features/obtencion_producto/application/dia_finder_service.dart
+++ lib/features/obtencion_producto/application/dia_finder_service.dart
@@ -1,46 +1,70 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../../../shared/data_entities/producto.dart';
+import '../../../shared/data_entities/tienda.dart';
 
 class DiaFinderService {
   final String marketUri = "https://www.dia.es/api/v1/search-back/search/reduced?q=%s&page=1";
   final String imageHost = "https://www.dia.es";
 
-  Future<List<Producto>> fetchProductsFromApi(String query) async {
-    final url = Uri.parse(marketUri.replaceFirst('%s', query));
-
-    try {
-      final response = await http.get(url);
-
-      if (response.statusCode == 200) {
-        final jsonData = json.decode(response.body);
-        return _parseProductList(jsonData);
-      } else {
-        throw Exception("Failed to load products");
-      }
-    } catch (e) {
-      print("Error fetching products: $e");
-      return [];
-    }
+  String getMarketUri(String query) {
+    return marketUri.replaceFirst("%s", query);
   }
 
-  List<Producto> _parseProductList(Map<String, dynamic> json) {
+  Future<List<Producto>> getProductList(String query) async {
     final List<Producto> productList = [];
-    final productsJsonList = json['search_items'] as List<dynamic>;
 
-    final facets = json['facets'];
+    try {
+      final url = getMarketUri(query);
+      final response = await http.get(Uri.parse(url));
 
-    for (final productJson in productsJsonList) {
-      final productObj = Producto.fromJson(
-        productJson,
-        imageHost: imageHost,
-        marketName: "DIA",
-        facets: facets,
-      );
-      productList.add(productObj);
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        final List productsJsonList = jsonResponse["search_items"];
+        final List facets = jsonResponse["facets"];
+
+        // Variables para almacenar los valores de gluten, lactosa y frutos secos
+        bool glutenFree = false;
+        bool lactoseFree = false;
+        bool nutsFree = false;
+
+        // Extraer información de los campos facets (alergenos)
+        for (var facet in facets) {
+          if (facet["field"] == "gluten_free") {
+            glutenFree = facet["filters"][0]["title"].toLowerCase() == "si";
+          } else if (facet["field"] == "lactose_free") {
+            lactoseFree = facet["filters"][0]["title"].toLowerCase() == "si";
+          } else if (facet["field"] == "nuts_free") {
+            nutsFree = facet["filters"][0]["title"].toLowerCase() == "si";
+          }
+        }
+
+        // Procesar la lista de productos
+        for (var productJson in productsJsonList) {
+          final pricesObj = productJson["prices"];
+          final product = Producto(
+            id: productJson["object_id"] ?? "",
+            tienda: Tienda.DIA,
+            marca: productJson["brand"] ?? "-",
+            precio: pricesObj["price"].toDouble(),
+            nombre: productJson["display_name"],
+            foto: productJson["image"].isNotEmpty
+                ? imageHost + productJson["image"]
+                : "",
+            alergenos: [glutenFree, lactoseFree, nutsFree],
+          );
+
+          productList.add(product);
+        }
+      } else {
+        print("Error fetching products: ${response.statusCode}");
+      }
+    } catch (e) {
+      print("Error: $e");
     }
 
     return productList;
   }
 }
+
 
--- lib/shared/data_entities/producto.dart
+++ lib/shared/data_entities/producto.dart
@@ -15,47 +15,20 @@ class Producto
   */
   final double precio;
   final String tienda;
+  final String marca;
   
   Producto({
     required this.id,
     required this.nombre,
-    required this.foto,
+    this.foto = "",
     required this.alergenos,
     required this.precio,
     required this.tienda,
+    required this.marca,
   });
 
-  // Método para convertir un JSON a un objeto Producto
-  factory Producto.fromJson(Map<String, dynamic> json, {required String imageHost, required String marketName, required Map<String, dynamic> facets}) {
-    // Obtener el estado de los alérgenos
-    bool glutenFree = _getAllergenStatus(facets, 'gluten_free');
-    bool lactoseFree = _getAllergenStatus(facets, 'lactose_free');
-    bool nutsFree = _getAllergenStatus(facets, 'nuts_free');
-
-    return Producto(
-      id: json['object_id'] ?? '', // Utiliza el campo adecuado del JSON
-      nombre: json['display_name'] ?? '',
-      precio: json['prices']['price'].toDouble(),
-      foto: imageHost + (json['image'] ?? ''),
-      tienda: marketName,
-      alergenos: [glutenFree, lactoseFree, nutsFree], // Asignación de alérgenos
-    );
-  }
-
-  // Método para obtener el estado de un alérgeno
-  static bool _getAllergenStatus(Map<String, dynamic> facets, String allergenKey) {
-    // Busca el facet correspondiente al alérgeno
-    final allergenFacet = facets[allergenKey];
-
-    if (allergenFacet != null) {
-      // Busca el filtro habilitado
-      final filter = allergenFacet['filters']
-          .firstWhere((filter) => filter['enabled'] == true, orElse: () => null);
-
-      if (filter != null) {
-        return filter['title'] == 'SI'; // Devuelve true si el producto es libre de alérgenos
-      }
-    }
-    return false; // Retorna false si no se encuentra el estado
+  @override
+  String toString() {
+    return 'Producto{id: $id, nombre: $nombre, precio: $precio, imagen: $foto, marca: $marca, glutenFree: ${alergenos[0]}, lactoseFree: ${alergenos[1]}, nutsFree: ${alergenos[2]}}';
   }
 }
