0ff2f8ab2416dbf73f187dba44edf571e81f7b1a
Nadia Albiol Moral <NALBMOR1@upv.edu.es>
2024-10-22 16:36:51 +0200

    arreglo comparador

--- lib/features/comparacion_productos/application/comparacion_producto.dart
+++ lib/features/comparacion_productos/application/comparacion_producto.dart
@@ -1,15 +1,18 @@
 import '../../../shared/data_entities/producto.dart';
 
-Producto? obtenerProductoMasBarato(List<Producto> productos) {
-  if (productos.isEmpty) return null;
+class Comparacion_producto
+{
+  static Producto? obtenerProductoMasBarato(List<Producto> productos) {
+    if (productos.isEmpty) return null;
 
-  Producto productoMasBarato = productos[0];
+    Producto productoMasBarato = productos[0];
 
-  for (var producto in productos) {
-    if (producto.precio < productoMasBarato.precio) {
-      productoMasBarato = producto;
+    for (var producto in productos) {
+      if (producto.precio < productoMasBarato.precio) {
+        productoMasBarato = producto;
+      }
     }
-  }
 
-  return productoMasBarato;
+    return productoMasBarato;
+  }
 }
