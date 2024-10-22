4db06e0f242be214d6c2111bbff1377d0886675b
ADRACHE <adrache@etsinf.upv.es>
2024-10-22 13:54:21 +0200

    Creación y modificación consum_finder_service.dart

--- lib/features/consum_finder/application/consum_finder_service.dart
+++ lib/features/consum_finder/application/consum_finder_service.dart
@@ -1,15 +1,19 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../../../shared/data_entities/producto.dart';
+import '../../../shared/data_entities/tienda.dart';
 
 class ConsumFinderService {
   final String marketUri = "https://tienda.consum.es/api/rest/V1.0/catalog/searcher/products?q=%s&limit=20&showRecommendations=false";
   final String imageHost = "https://www.tienda.consum.es";
 
-  Future<List<Producto>> fetchProductsFromApi(String query) async {
-    final url = Uri.parse(marketUri.replaceFirst('%s', query));
+  Uri getMarketUri(String query) {
+    return Uri.parse(marketUri.replaceFirst('%s', query));
+  }
 
+  Future<List<Producto>> fetchProductsFromApi(String query) async {
     try {
+      final url = getMarketUri(query);
       final response = await http.get(url);
 
       if (response.statusCode == 200) {
@@ -26,7 +30,23 @@ class ConsumFinderService {
 
   Future<List<Producto>> _parseProductList(Map<String, dynamic> json) async {
     final List<Producto> productList = [];
+    final List productsJsonList = json["search_items"];
 
-    return productList;
+    //crear lista de productos
+    for (var productJson in productsJsonList) {
+      final pricesObj = productJson["prices"];
+      final product = Producto(
+          id: productJson["object_id"] ?? "",
+          tienda: Tienda.CONSUM,
+          marca: productJson["brand"] ?? "-",
+          precio: pricesObj["price"].toDouble(),
+          nombre: productJson["display_name"],
+          foto: productJson["image"].isNotEmpty ? imageHost +
+              productJson["image"] : "",
+          alergenos: [false, false, false]);
+
+      productList.add(product);
+      return productList;
+    }
   }
 }
