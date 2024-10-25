76cfa81755678321e4137e1af21c788c0ca98038
ADRACHE <adrache@etsinf.upv.es>
2024-10-25 13:50:03 +0200

    Creación test_consum_finder y aseguramiento de su funcionamiento

--- lib/features/obtencion_producto/application/consum_finder_service.dart
+++ lib/features/obtencion_producto/application/consum_finder_service.dart
@@ -17,8 +17,10 @@ class ConsumFinderService {
       final response = await http.get(url);
 
       if (response.statusCode == 200) {
-        final jsonData = json.decode(response.body);
-        return _parseProductList(jsonData);
+        final jsonResp = json.decode(response.body);
+        final productsJsonMap = jsonResp["catalog"];
+        final productsJsonList = productsJsonMap["products"];
+        return _parseProductList(productsJsonList);
       } else {
         throw Exception("Failed to load products");
       }
@@ -28,21 +30,26 @@ class ConsumFinderService {
     }
   }
 
-  Future<List<Producto>> _parseProductList(Map<String, dynamic> json) async {
+  Future<List<Producto>> _parseProductList(List productsJsonList) async {
     final List<Producto> productList = [];
-    final List productsJsonList = json["products"];
-
-    //crear lista de productos
+//crear lista de productos
     for (var productJson in productsJsonList) {
-      final pricesObj = productJson["prices"];
+      final currProduct = productJson["productData"];
+      final marca = currProduct["brand"];
+      final priceMap = productJson["priceData"];
+      final pricesObj = priceMap["prices"];
+      final priceVal = pricesObj[0];
+      final priceCent = priceVal["value"];
+
+//crear producto
       final product = Producto(
-          id: productJson["object_id"] ?? "",
+          id: currProduct["id"] ?? "",
           tienda: Tienda.CONSUM,
-          marca: productJson["brand"] ?? "-",
-          precio: pricesObj["price"].toDouble(),
-          nombre: productJson["display_name"],
-          foto: productJson["image"].isNotEmpty ? imageHost +
-              productJson["image"] : "",
+          marca: marca["name"] ?? "-",
+          precio: priceCent["centAmount"],
+          nombre: currProduct["name"],
+          foto: currProduct["imageURL"].isNotEmpty ? imageHost +
+              currProduct["imageURL"] : "",
           alergenos: [false, false, false]);
 
       productList.add(product);
