5c0581c50ad9ff725297fd9f3e9afda7ead0eaa1
QuinoCode <joaquinangelvalienteruiz@gmail.com>
2024-11-01 17:31:45 +0100

    Removed the class from 'comparación de producto'

--- lib/features/comparacion_productos/application/comparacion_producto.dart
+++ lib/features/comparacion_productos/application/comparacion_producto.dart
@@ -1,23 +1,17 @@
-import '../../../shared/data_entities/producto.dart';
+import 'package:prizo/shared/data_entities/producto.dart';
 
-class Comparacion_producto
-{
-  static Producto? obtenerProductoMasBarato(List<Producto> productos) {
-    if (productos.isEmpty) return null;
-
-    Producto productoMasBarato = productos[0];
-
-    for (var producto in productos) {
-      if (producto.precio < productoMasBarato.precio) {
-        productoMasBarato = producto;
-      }
+Producto? obtenerProductoMasBarato(List<Producto> productos) {
+  if (productos.isEmpty) return null;
+  Producto productoMasBarato = productos[0];
+  for (var producto in productos) {
+    if (producto.precio < productoMasBarato.precio) {
+      productoMasBarato = producto;
     }
-
-    return productoMasBarato;
   }
+  return productoMasBarato;
+}
 
-  //ordena una lista de productos en base a su precio de menor a mayor
-  static void ordenarProductosPorPrecio(List<Producto> productos) {
-    productos.sort((a, b) => a.precio.compareTo(b.precio));
-  }
+//ordena una lista de productos en base a su precio de menor a mayor
+void ordenarProductosPorPrecio(List<Producto> productos) {
+  productos.sort((a, b) => a.precio.compareTo(b.precio));
 }
