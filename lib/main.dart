fb05b37ce535358b8f127cf06a731a9340c29d52
Alejandro Girardi Candel <AGIRCAN@upv.edu.es>
2024-11-03 18:12:55 +0100

    search main

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './features/lista_compra/presentation/lista_compra_interfaz.dart';
+import './features/productsearch/productsearch.ui.dart'; // Asegúrate de que la ruta sea correcta
 
 void main() {
   runApp(const Prizo());
 }
 
-class Prizo extends StatelessWidget 
-{
+class Prizo extends StatelessWidget {
   const Prizo({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => PrizoState(),
-      child:  MaterialApp(
-        home: ListaCompraInterfaz(displayText: 'Hello World'),
+      child: MaterialApp(
+        home: ProductSearchScreen(), // Aquí se cambia ListaCompraInterfaz por ProductSearchScreen
       ),
     );
   }
 }
-class PrizoState extends ChangeNotifier 
-{
+
+class PrizoState extends ChangeNotifier {
   var displayTest = 'Hello World';
 }
