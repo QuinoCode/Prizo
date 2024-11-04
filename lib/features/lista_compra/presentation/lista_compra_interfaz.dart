ef8f688a7d01b63104613e88d14b4d53a2a97cde
Alejandro Girardi Candel <AGIRCAN@upv.edu.es>
2024-11-04 20:42:24 +0100

    lista de la compra

--- lib/features/lista_compra/presentation/lista_compra_interfaz.dart
+++ lib/features/lista_compra/presentation/lista_compra_interfaz.dart
@@ -1,25 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:prizo/shared/data_entities/lista_compra.dart';
 
-class ListaCompraInterfaz extends StatelessWidget
-{
-  String displayText;
+class ListaCompraInterfaz extends StatelessWidget {
+  final ListaCompra listaCompra;
 
-  ListaCompraInterfaz({super.key, required this.displayText});
+  ListaCompraInterfaz({super.key, required this.listaCompra});
 
   @override
   Widget build(BuildContext context) {
-        return Scaffold(
-          body: Container(
-            color: Theme.of(context).colorScheme.primaryContainer,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(displayText),
-                ],
-              ),
-            ),
-            ),
-          );
-      }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tu Lista de la Compra'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: listaCompra.productos.isEmpty
+            ? Center(
+          child: Text('Tu lista de la compra está vacía.'),
+        )
+            : ListView.builder(
+          itemCount: listaCompra.productos.length,
+          itemBuilder: (context, index) {
+            final producto = listaCompra.productos[index];
+            return ListTile(
+              title: Text(producto.nombre),
+              subtitle: Text('${producto.tienda} - €${producto.precio.toStringAsFixed(2)}'),
+            );
+          },
+        ),
+      ),
+    );
+  }
 }
