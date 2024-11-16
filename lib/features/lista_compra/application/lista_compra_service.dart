1ace8dedd6a4543f7de9e3eb611cd3c04dd68396
José Ramón <jrtormoarmelar@gmail.com>
2024-11-16 19:55:26 +0100

    Creación "ListaCompraAuxiliar" en "lista_compra_service.dart"

--- lib/features/lista_compra/application/lista_compra_service.dart
+++ lib/features/lista_compra/application/lista_compra_service.dart
@@ -1,6 +1,16 @@
 import 'package:prizo/shared/data_entities/lista_compra.dart';
 import 'package:prizo/shared/data_entities/producto.dart';
 
+class ListaCompraAuxiliar {
+  List<Producto> productos;
+  List<int> cantidades;
+
+  ListaCompraAuxiliar({
+    required this.productos,
+    required this.cantidades,
+  });
+}
+
 class ListaCompraService {
 
   void addProduct(ListaCompra list, Producto product){
