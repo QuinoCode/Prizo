3283ecda10351dfbb97bbe8884a0f7116069dc10
José Ramón <jrtormoarmelar@gmail.com>
2024-11-17 02:32:45 +0100

    Ver imagen en lista de compra

--- lib/features/lista_compra/presentation/lista_compra_interfaz.dart
+++ lib/features/lista_compra/presentation/lista_compra_interfaz.dart
@@ -15,14 +15,25 @@ class ListaCompraInterfaz extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: listaCompra.productos.isEmpty
-            ? Center(
-          child: Text('Tu lista de la compra está vacía.'),
-        )
+            ? Center(child: Text('Tu lista de la compra está vacía.'))
             : ListView.builder(
           itemCount: listaCompra.productos.length,
           itemBuilder: (context, index) {
             final producto = listaCompra.productos[index];
+            final imageUrl = producto.foto; // Obtener la URL de la imagen
+
             return ListTile(
+              leading: producto.foto.isNotEmpty
+                  ? Image.network(
+                imageUrl,
+                width: 50,
+                height: 50,
+                errorBuilder: (context, error, stackTrace) {
+                  print('Error loading image: $error');
+                  return Icon(Icons.broken_image); // Mostrar un icono en caso de error
+                },
+              )
+                  : Icon(Icons.image_not_supported),
               title: Text(producto.nombre),
               subtitle: Text('${producto.tienda} - €${producto.precio.toStringAsFixed(2)}'),
             );
