3d0102a8f76a74d82faccad492d8ac653779615f
José Ramón <jrtormoarmelar@gmail.com>
2024-11-26 19:48:22 +0100

    Creación de "ListaCompra2" en "lista_compra.dart"

--- lib/shared/data_entities/lista_compra.dart
+++ lib/shared/data_entities/lista_compra.dart
@@ -12,3 +12,16 @@ class ListaCompra
     required this.productos,
   });
 }
+
+class ListaCompra2
+{
+  final String id;
+  final String usuario;
+  final List<(Producto, int)> productos;
+
+  ListaCompra2({
+    required this.id,
+    required this.usuario,
+    required this.productos,
+  });
+}
