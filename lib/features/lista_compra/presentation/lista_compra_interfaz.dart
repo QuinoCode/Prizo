57946a0d73d15b36cde86cf6c4654832eadda09e
José Ramón <jrtormoarmelar@gmail.com>
2024-11-26 23:27:12 +0100

    Botón papelera y número de instancias en "lista_compra_interfaz.dart"

--- lib/features/lista_compra/presentation/lista_compra_interfaz.dart
+++ lib/features/lista_compra/presentation/lista_compra_interfaz.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:prizo/shared/data_entities/producto.dart';
 import 'package:prizo/shared/data_entities/lista_compra.dart';
+import 'package:prizo/shared/data_entities/producto.dart';
 import 'package:prizo/features/lista_compra/application/lista_compra_service.dart';
 
-class ListaCompraInterfaz extends StatelessWidget {
-  final ListaCompra2 listaCompra; // Cambiado a ListaCompra2
+class ListaCompraInterfaz extends StatefulWidget {
+  final ListaCompra2 listaCompra;
 
   ListaCompraInterfaz({super.key, required this.listaCompra});
+
+  @override
+  _ListaCompraInterfazState createState() => _ListaCompraInterfazState();
+}
+
+class _ListaCompraInterfazState extends State<ListaCompraInterfaz> {
+  final ListaCompraService listaCompraService = ListaCompraService();
 
   @override
   Widget build(BuildContext context) {
@@ -16,19 +23,54 @@ class ListaCompraInterfaz extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: listaCompra.productos.isEmpty
+        child: widget.listaCompra.productos.isEmpty
             ? Center(child: Text('Tu lista de la compra está vacía.'))
             : ListView.builder(
-          itemCount: listaCompra.productos.length,
+          itemCount: widget.listaCompra.productos.length,
           itemBuilder: (context, index) {
-            final productoTuple = listaCompra.productos[index];
+            final productoTuple = widget.listaCompra.productos[index];
             final producto = productoTuple.$1;
             final cantidad = productoTuple.$2;
 
             return ListTile(
-              title: Text(producto.nombre),
+              leading: producto.foto.isNotEmpty
+                  ? Image.network(
+                producto.foto,
+                width: 50,
+                height: 50,
+                errorBuilder: (context, error, stackTrace) {
+                  print('Error loading image: $error');
+                  return Icon(Icons.broken_image);
+                },
+              )
+                  : Icon(Icons.image_not_supported),
+              title: Text(
+                producto.nombre,
+                style: TextStyle(fontSize: 16), // tamaño del texto del producto
+              ),
               subtitle: Text(
-                '${producto.tienda} - €${producto.precio.toStringAsFixed(2)} (Cantidad: $cantidad)',
+                '${producto.tienda} - €${producto.precio.toStringAsFixed(2)}',
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    '$cantidad',
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      setState(() {
+                        listaCompraService.removeProduct(
+                            widget.listaCompra, producto);
+                      });
+                    },
+                  ),
+                ],
               ),
             );
           },
