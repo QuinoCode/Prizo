fbbc42da0126e4e6a6c7fdd774b1f975881434dd
José Ramón <jrtormoarmelar@gmail.com>
2024-11-26 21:01:23 +0100

    Eliminar "crearListaCompraAuxiliar" y "addProductAux" de "lista_compra_service.dart"

--- lib/features/lista_compra/application/lista_compra_service.dart
+++ lib/features/lista_compra/application/lista_compra_service.dart
@@ -68,44 +68,5 @@ class ListaCompraService {
       return list.productos[i].$2;
     }
     return -1;
-  }
-
-  void addProductAux(ListaCompraAuxiliar list, Producto product) {
-    /* Buscar si el producto ya está en la lista */
-    int index = list.productos.indexWhere((p) => p.id == product.id);
-
-    if (index != -1) {
-      /* Si el producto ya existe, incrementar la cantidad */
-      list.cantidades[index]+=1;
-    } else {
-      /* Si el producto no existe, agregarlo con cantidad inicial de 1 */
-      list.productos.add(product);
-      list.cantidades.add(1);
-    }
   }//setProductQuantity
-
-  ListaCompraAuxiliar crearListaCompraAuxiliar(ListaCompra listaCompra) {
-    /*Mapa para contar la cantidad de cada producto*/
-    Map<String, int> contadorProductos = {};
-    List<Producto> productosUnicos = [];
-
-    for (var producto in listaCompra.productos) {
-      if (contadorProductos.containsKey(producto.id)) {
-        contadorProductos[producto.id] = contadorProductos[producto.id]! + 1;
-      } else {
-        contadorProductos[producto.id] = 1;
-        productosUnicos.add(producto); /*Agregar producto único*/
-      }
-    }
-    /*Crear las listas de productos y cantidades*/
-    List<int> cantidades = productosUnicos.map((producto) => contadorProductos[producto.id]!).toList();
-
-    ListaCompraAuxiliar lista = new ListaCompraAuxiliar();
-    for (var producto in listaCompra.productos) {
-      addProductAux(lista, producto);
-    }
-
-    return ListaCompraAuxiliar(productos: productosUnicos, cantidades: cantidades);
-  }
-
 }
