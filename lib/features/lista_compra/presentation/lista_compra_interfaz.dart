dec3b6cb02656a0240cc4d5b18934aa3f26a4f07
José Ramón <jrtormoarmelar@gmail.com>
2024-11-27 09:08:38 +0100

    Poder ver el precio total de la compra en "lista_compra_interfaz.dart"

--- lib/features/lista_compra/presentation/lista_compra_interfaz.dart
+++ lib/features/lista_compra/presentation/lista_compra_interfaz.dart
@@ -15,32 +15,30 @@ class ListaCompraInterfaz extends StatefulWidget {
 class _ListaCompraInterfazState extends State<ListaCompraInterfaz> {
   final ListaCompraService listaCompraService = ListaCompraService();
 
-  // Método para mostrar el cuadro de diálogo de confirmación
-  Future<void> _showConfirmDialog(
-      BuildContext context, Producto producto) async {
+  /* Método para mostrar el cuadro de diálogo de confirmación */
+  Future<void> _showConfirmDialog(BuildContext context, Producto producto) async {
     return showDialog<void>(
       context: context,
       barrierDismissible: false, // Evita cerrar el diálogo tocando fuera de él
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text('¿Eliminar producto?'),
-          content: Text(
-              '¿Estás seguro de que deseas eliminar el producto ${producto.nombre}?'),
+          content: Text('¿Estás seguro de que deseas eliminar el producto ${producto.nombre}?'),
           actions: <Widget>[
             TextButton(
               onPressed: () {
-                // Cerrar el diálogo sin hacer nada
+                /* Cerrar el diálogo sin hacer nada */
                 Navigator.of(context).pop();
               },
               child: Text('Cancelar'),
             ),
             TextButton(
               onPressed: () {
-                // Eliminar el producto completo de la lista
+                /* Eliminar el producto completo de la lista */
                 setState(() {
                   listaCompraService.removeProduct(widget.listaCompra, producto);
                 });
-                Navigator.of(context).pop(); // Cerrar el cuadro de diálogo
+                Navigator.of(context).pop(); /* Cerrar el cuadro de diálogo */
               },
               child: Text('Eliminar'),
             ),
@@ -52,6 +50,9 @@ class _ListaCompraInterfazState extends State<ListaCompraInterfaz> {
 
   @override
   Widget build(BuildContext context) {
+    /* Calcular el precio total */
+    double totalPrice = listaCompraService.getTotalPrice(widget.listaCompra);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Tu Lista de la Compra'),
@@ -60,71 +61,92 @@ class _ListaCompraInterfazState extends State<ListaCompraInterfaz> {
         padding: const EdgeInsets.all(16.0),
         child: widget.listaCompra.productos.isEmpty
             ? Center(child: Text('Tu lista de la compra está vacía.'))
-            : ListView.builder(
-          itemCount: widget.listaCompra.productos.length,
-          itemBuilder: (context, index) {
-            final productoTuple = widget.listaCompra.productos[index];
-            final producto = productoTuple.$1;
-            final cantidad = productoTuple.$2;
-            final totalPrice = listaCompraService.getPrice(
-                widget.listaCompra, producto);
+            : Column(
+          children: [
+            /* Lista de productos */
+            Expanded(
+              child: ListView.builder(
+                itemCount: widget.listaCompra.productos.length,
+                itemBuilder: (context, index) {
+                  final productoTuple = widget.listaCompra.productos[index];
+                  final producto = productoTuple.$1;
+                  final cantidad = productoTuple.$2;
+                  final totalPriceForProduct = listaCompraService.getPrice(widget.listaCompra, producto);
 
-            return ListTile(
-              leading: producto.foto.isNotEmpty
-                  ? Image.network(
-                producto.foto,
-                width: 50,
-                height: 50,
-                errorBuilder: (context, error, stackTrace) {
-                  print('Error loading image: $error');
-                  return Icon(Icons.image_not_supported);
+                  return ListTile(
+                    leading: producto.foto.isNotEmpty
+                        ? Image.network(
+                      producto.foto,
+                      width: 50,
+                      height: 50,
+                      errorBuilder: (context, error, stackTrace) {
+                        print('Error loading image: $error');
+                        return Icon(Icons.image_not_supported);
+                      },
+                    )
+                        : Icon(Icons.image_not_supported),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        /* Botón para eliminar una instancia con confirmación */
+                        IconButton(
+                          icon: Icon(Icons.remove_circle_outline),
+                          onPressed: () {
+                            /* Mostrar el cuadro de confirmación antes de eliminar */
+                            _showConfirmDialog(context, producto);
+                          },
+                        ),
+                        /* Cantidad */
+                        Text(
+                          '$cantidad',
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        /* Botón para agregar una instancia */
+                        IconButton(
+                          icon: Icon(Icons.add_circle_outline),
+                          onPressed: () {
+                            setState(() {
+                              listaCompraService.addInstance(widget.listaCompra, producto);
+                            });
+                          },
+                        ),
+                        /* Precio total del producto */
+                        Text(
+                          '${totalPriceForProduct.toStringAsFixed(2)} €',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        /* Botón de papelera para eliminar el producto completo */
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            /* Mostrar el cuadro de confirmación antes de eliminar */
+                            _showConfirmDialog(context, producto);
+                          },
+                        ),
+                      ],
+                    ),
+                  );
                 },
-              )
-                  : Icon(Icons.image_not_supported),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
+              ),
+            ),
+            /* Muestra el precio total de todos los productos */
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  // Botón para eliminar una instancia con confirmación
-                  IconButton(
-                    icon: Icon(Icons.remove_circle_outline),
-                    onPressed: () {
-                      // Mostrar el cuadro de confirmación antes de eliminar
-                      _showConfirmDialog(context, producto);
-                    },
-                  ),
-                  // Cantidad
                   Text(
-                    '$cantidad',
-                    style: TextStyle(
-                        fontSize: 20, fontWeight: FontWeight.bold),
+                    'Total:',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                  // Botón para agregar una instancia
-                  IconButton(
-                    icon: Icon(Icons.add_circle_outline),
-                    onPressed: () {
-                      setState(() {
-                        listaCompraService.addInstance(
-                            widget.listaCompra, producto);
-                      });
-                    },
-                  ),
-                  // Precio total del producto
                   Text(
                     '${totalPrice.toStringAsFixed(2)} €',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  // Botón de papelera para eliminar el producto completo
-                  IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () {
-                      // Mostrar el cuadro de confirmación antes de eliminar
-                      _showConfirmDialog(context, producto);
-                    },
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.green),
                   ),
                 ],
               ),
-            );
-          },
+            ),
+          ],
         ),
       ),
     );
