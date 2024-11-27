8d628d1fe92f6f83216dfc5ed3b735252ef75b4b
José Ramón <jrtormoarmelar@gmail.com>
2024-11-27 08:47:09 +0100

    Creación e integración de "updatedProduct" en"lista_favoritos_service.dart"

--- lib/features/lista_favoritos/application/lista_favoritos_service.dart
+++ lib/features/lista_favoritos/application/lista_favoritos_service.dart
@@ -8,8 +8,16 @@ class ListaFavoritosService {
   }
 
   void addProduct(ListaFavoritos list, Producto product) {
-    bool exists = list.productos.any((p) => sameProduct(p, product));
-    if (!exists) {
+    int index = list.productos.indexWhere((p) => sameProduct(p, product));
+
+    if (index != -1) {
+      /* El producto existe, verificamos si está actualizado */
+      if (updatedProduct(list.productos[index], product)) {
+        /* Reemplazar el producto existente con el nuevo */
+        list.productos[index] = product;
+      }
+    } else {
+      /* Si no existe, se agrega el nuevo producto */
       list.productos.add(product);
     }
   }
@@ -21,6 +29,14 @@ class ListaFavoritosService {
         productA.alergenos == productB.alergenos &&
            productA.tienda == productB.tienda &&
             productA.marca == productB.marca;
+  }
+
+  bool updatedProduct(Producto productA, Producto productB) {
+    return      productA.foto != productB.foto ||
+              productA.precio != productB.precio ||
+        productA.precioMedida != productB.precioMedida ||
+              productA.oferta != productB.oferta ||
+        productA.precioOferta != productB.precioOferta;
   }
 
 }
