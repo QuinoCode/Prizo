202b2b9323fc2108d16e48b9396baa0a954f6524
ADRACHE <adrache@etsinf.upv.es>
2024-12-12 19:42:01 +0100

    Funciona prueba llamar_busqueda

--- lib/features/distancia_tienda/shop_distance.dart
+++ lib/features/distancia_tienda/shop_distance.dart
@@ -1,12 +1,13 @@
 import "dart:convert";
 import 'package:http/http.dart' as http;
+import 'dart:io';
 import 'package:location/location.dart';
 
 class ShopDistance {
-  final String HEREapi = "https://discover.search.hereapi.com/v1/discover?at=%a&q=%b&apiKey=%c";
+  final String HEREapi = "https://discover.search.hereapi.com/v1/discover?at=%a&q=%b&limit=3&apiKey=%c";
   final String HEREkey = "cWq-x6-p6dQ007TjTfvpfuhKsQKFyDBtzJZ8dbjEH6Y";
 
-  Location location = new Location();
+  Location location = Location();
 
   /*Future<bool> _serviceEnabled() async {
     var service = await location.serviceEnabled();
@@ -30,23 +31,23 @@ class ShopDistance {
     return permission;
   }
 */
-  Uri getFullUri(LocationData coords, String query){
-    String s = HEREapi.replaceFirst('%a', "${coords.latitude},${coords.longitude}");
-    s = HEREapi.replaceFirst('%b', query);
-    s = HEREapi.replaceFirst('%c', HEREkey);
-    return Uri.parse(s);
+  String getFullUri(LocationData coords, String query){
+    String s = HEREapi.replaceFirst('%a', '${coords.latitude},${coords.longitude}');
+    s = s.replaceFirst('%b', query);
+    s = s.replaceFirst('%c', HEREkey);
+    return Uri.encodeFull(s);
   }
 
   Future<String> fetchLocationsAPI(String query) async {
     try {
       //_serviceEnabled();
       //_permissionGranted();
+      HttpOverrides.global = MyHttpOverrides();
       final coords = await location.getLocation();
       final url = getFullUri(coords, query);
       print(url);
-      final response = await http.get(url);
-
-      if (response.statusCode == 200) {
+      var response = await http.get(Uri.parse(url), headers:{"Accept":"application/json", "Accept-Encoding":"gzip"});
+      if (response. statusCode == 200) {
         final jsonResp = json.decode(response.body);
         final locationJsonMap = jsonResp["items"];
         return locationJsonMap[0]["distance"];
@@ -59,4 +60,11 @@ class ShopDistance {
     }
   }
 
+}
+class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
 }
