3086c6aad5de166bd8b57f1a3d935e0355b5ddc0
QuinoCode <joaquinangelvalienteruiz@gmail.com>
2024-12-15 19:59:07 +0100

    Solved inconsistencies to the scanner

--- lib/features/escaner/application/scanner_service.dart
+++ lib/features/escaner/application/scanner_service.dart
@@ -1,42 +1,77 @@
 import 'package:flutter/foundation.dart';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:prizo/features/obtencion_producto/application/ean_finder.dart';
 import 'package:prizo/features/obtencion_producto/application/finder_wrapper.dart';
 import 'package:prizo/shared/data_entities/producto.dart';
+import 'package:prizo/features/escaner/presentation/interfaz_scanner.dart';
 
 
-MobileScanner createScanner(BuildContext context) {
+MobileScanner createScanner(BuildContext context, EanFinder eanFinder) {
+  bool lockOpen = true;
    MobileScanner scanner = MobileScanner(
   controller: MobileScannerController(
-      detectionSpeed: DetectionSpeed.noDuplicates,
+      detectionSpeed: DetectionSpeed.normal,
       returnImage: true
     ),
-    onDetect:(capture){detected(capture, context);}
+    onDetect:(capture) async{
+      if (lockOpen){
+        lockOpen = false;
+        await detected(capture, context, eanFinder);
+        lockOpen = true;
+      }
+    }
 );
   return scanner;
   
 }
 
-//TODO Make some kind of feedback in order for the user to know if something successfully scanned and we're waiting just for the http process to resolve
-void detected(capture, context) async {
+Future<void> detected(capture, BuildContext context, EanFinder eanFinder) async {
       final List<Barcode> barcodes = capture.barcodes;
       final Uint8List? image = capture.image;
       List<Producto?>? products;
       for (final barcode in barcodes) {
-        products = await getProductFromScan(barcode.rawValue);
+        if (image != null) { 
+          showDialog(context: context, builder: (context) =>
+            feedbackSuccessfulScan(image)
+          ); 
+        }
+        products = await getProductFromScan(context, barcode.rawValue, eanFinder);
+        if (products == null) {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("Error"),
+                content: const Text("Item not found in the database"),
+                actions: [
+                  TextButton(
+                    child: const Text("OK"),
+                    onPressed: () {
+                      Navigator.of(context).pop(); // Close the dialog
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        }
+        else { 
+          showDialog(context: context, builder: (context) =>
+            createAlertDialog(products!, image!)
+          ); 
+        };
         if (products == null) {print("No se encontró el producto en ningún supermercado");}
       }
-      if (image != null) { 
-        showDialog(context: context, builder: (context) =>
-          createAlertDialog(products!, image)
-        ); 
-      }
+}
+AlertDialog feedbackSuccessfulScan(Uint8List image){
+  return AlertDialog(title: const Text("Escaneado completo"), content: Image(image: MemoryImage(image)));
 }
 AlertDialog createAlertDialog(List<Producto?> products, Uint8List image){
   String nonNullProducts = ""; 
   for (int i = 0; i < products.length; i++){
-    if (products[i] != null) {nonNullProducts += "${products[i]!.tienda}: ${products[i]!.precio} \n";}
+    if (products[i] != null) {nonNullProducts += "${products[i]!.tienda}: ${products[i]!.precioOferta} \n";}
   }
 
   if (nonNullProducts.isEmpty) {nonNullProducts = "No products were found";}
@@ -48,8 +83,7 @@ AlertDialog createAlertDialog(List<Producto?> products, Uint8List image){
   ));
 }
 
-Future<List<Producto?>?>  getProductFromScan(String? ean) async {
+Future<List<Producto?>?>  getProductFromScan(BuildContext context, String? ean, EanFinder eanFinder) async {
   if (ean == null) return null;
-  EanFinder finder = EanFinder();
-  return await finder.getProductList(ean);
+  return await eanFinder.getProductList(ean);
 }
--- lib/features/escaner/presentation/interfaz_scanner.dart
+++ lib/features/escaner/presentation/interfaz_scanner.dart
@@ -1,5 +1,6 @@
 import 'package:prizo/features/escaner/application/scanner_service.dart';
 import 'package:flutter/material.dart';
+import 'package:prizo/features/obtencion_producto/application/ean_finder.dart';
 
 class ScannerInterface extends StatefulWidget {
 	final String title;
@@ -10,21 +11,49 @@ class ScannerInterface extends StatefulWidget {
 }
 
 class _ScannerInterfaceState extends State<ScannerInterface> {
-	 @override
-	 Widget build(BuildContext context) {
-		 return Scaffold(
-			 appBar: AppBar(
-				 title: Text(widget.title),
-				 centerTitle: true,
-			 ),
-			 body: Center(
-				 child: SizedBox(
-					 height: 200,
-					 width: 350,
-					 child: createScanner(context),
-				 ),
-			 ),
-		 );
-	 }
+  late EanFinder eanFinder;
+
+  @override
+  void initState(){
+    super.initState();
+    eanFinder = EanFinder(onError: noProductFoundOnEANDatabase);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+	   appBar: AppBar(
+		   title: Text(widget.title),
+		   centerTitle: true,
+	   ),
+	   body: Center(
+		   child: SizedBox(
+			   height: 200,
+			   width: 350,
+			   child: createScanner(context, eanFinder),
+		   ),
+	   ),
+    );
+  }
+  void noProductFoundOnEANDatabase(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+	return AlertDialog(
+	  title: const Text("Error"),
+	  content: Text(message),
+	  actions: [
+	    TextButton(
+	      child: const Text("OK"),
+	      onPressed: () {
+		Navigator.of(context).pop(); // Close the dialog
+	      },
+	    ),
+	  ],
+	);
+      },
+    );
+  }
+
 }
+
 
