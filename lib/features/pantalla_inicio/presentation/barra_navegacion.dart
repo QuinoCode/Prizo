01e6c7accf98c0424f4836dfb621f8f95154c584
José Ramón <jrtormoarmelar@gmail.com>
2025-01-24 20:58:20 +0100

    Aplicar actualización de Nadia

--- lib/features/pantalla_inicio/presentation/barra_navegacion.dart
+++ lib/features/pantalla_inicio/presentation/barra_navegacion.dart
@@ -33,8 +33,6 @@ class _BarraNavegacionState extends State<BarraNavegacion> {
           Positioned.fill(
             child: _screens[_currentIndex],
           ),
-
-
           Positioned(
             bottom: 16,
             left: 16,
@@ -46,56 +44,73 @@ class _BarraNavegacionState extends State<BarraNavegacion> {
                 borderRadius: BorderRadius.circular(50), // Forma ovalada
                 border: Border.all(
                   color: Color(0xFF95B3FF),
-                  width: 2,
+                  width: 3,
                 ),
               ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(30),
-                child: BottomNavigationBar(
-                  type: BottomNavigationBarType.fixed,
-                  currentIndex: _currentIndex,
-                  onTap: _onTabTapped,
-                  items: const [
-                    BottomNavigationBarItem(
-                      icon: const ImageIcon(
-                        AssetImage('assets/icons/casa_icono.png'),
-                        size: 24,
-                      ),
-                      label: '', // Sin texto debajo del icono
-                    ),
-                    BottomNavigationBarItem(
-                      icon: const ImageIcon(
-                        AssetImage('assets/icons/lupa_icono.png'),
-                        size: 20,
-                      ),
-                      label: '',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: const ImageIcon(
-                        AssetImage('assets/icons/listas_icono.png'),
-                        size: 22,
-                      ),
-                      label: '',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: const ImageIcon(
-                        AssetImage('assets/icons/usuario_icono.png'),
-                        size: 22,
-                      ),
-                      label: '',
-                    ),
-                  ],
-                  showSelectedLabels: false,
-                  showUnselectedLabels: false,
-                  selectedItemColor: Color(0xFF95B3FF), // Color del ícono seleccionado
-                  unselectedItemColor: Colors.black, // Color del ícono no seleccionado
-                  elevation: 0, // Sin sombra
-                  backgroundColor: Colors.transparent, // Fondo transparente
-                ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _buildNavItem(
+                    index: 0,
+                    iconPath: 'assets/icons/casa_icono.png',
+                    iconSize: 24,
+                  ),
+                  _buildNavItem(
+                    index: 1,
+                    iconPath: 'assets/icons/lupa_icono.png',
+                    iconSize: 20,
+                  ),
+                  _buildNavItem(
+                    index: 2,
+                    iconPath: 'assets/icons/listas_icono.png',
+                    iconSize: 22,
+                  ),
+                  _buildNavItem(
+                    index: 3,
+                    iconPath: 'assets/icons/usuario_icono.png',
+                    iconSize: 22,
+                  ),
+                ],
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildNavItem({
+    required int index,
+    required String iconPath,
+    required double iconSize,
+  }) {
+    final isSelected = _currentIndex == index;
+
+    return GestureDetector(
+      onTap: () => _onTabTapped(index),
+      child: Container(
+        width: 86,
+        height: 52,
+        alignment: Alignment.center,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            if (isSelected)
+              Container(
+                width: 86,
+                height: 52,
+                decoration: BoxDecoration(
+                  color: Color(0xFF95B3FF).withOpacity(1.0),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ImageIcon(
+              AssetImage(iconPath),
+              size: iconSize,
+              color: Colors.black,
+            ),
+          ],
+        ),
       ),
     );
   }
