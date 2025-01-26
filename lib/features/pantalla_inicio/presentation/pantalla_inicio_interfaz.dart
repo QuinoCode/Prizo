392c4b0e096c6b2d8c66d5f67af91cc1a6652af0
Nadia Albiol Moral <NALBMOR1@upv.edu.es>
2025-01-26 18:58:09 +0100

    foto ofertas contain

--- lib/features/pantalla_inicio/presentation/pantalla_inicio_interfaz.dart
+++ lib/features/pantalla_inicio/presentation/pantalla_inicio_interfaz.dart
@@ -277,7 +277,7 @@ class _PantallaInicioState extends State<PantallaInicio> {
                                         borderRadius: BorderRadius.circular(width * 0.03),
                                         child: Image.network(
                                           producto.foto,
-                                          fit: BoxFit.fill,
+                                          fit: BoxFit.contain,
                                           errorBuilder: (context, error, stackTrace) =>
                                               Icon(Icons.image_not_supported),
                                         ),
